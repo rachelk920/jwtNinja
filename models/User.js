5212ea77c6860e25fc1a37e8d9c4e3c09656d417
Rachel <rmshinski@gmail.com>
2021-05-31 15:56:49 -0400

    homework

--- models/User.js
+++ models/User.js
@@ -3,30 +3,38 @@ const { isEmail } = require('validator');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: [true, 'Please enter valid email'],
-        unique: true,
-        lowercase: true,
-        validate: [isEmail, 'Please enter valid email']
-    },
-    password: {
-        type: String,
-        required: [true, 'Please enter valid password'],
-        minLength: [6, 'Minimum length is 6 characters']
-    },
+  email: {
+    type: String,
+    required: [true, 'Please enter an email'],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, 'Please enter a valid email']
+  },
+  password: {
+    type: String,
+    required: [true, 'Please enter a password'],
+    minlength: [6, 'Minimum password length is 6 characters'],
+  }
 });
 
-userSchema.post('save', function(doc, next){
-    console.log('new user was created and saved', doc);
-    next();
+
+userSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
-userSchema.pre('save', async function(next){
-    const salt = await bcrypt.genSalt();
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-})
+userSchema.statics.login = async function(email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error('incorrect password');
+  }
+  throw Error('incorrect email');
+};
 
 const User = mongoose.model('user', userSchema);
 
